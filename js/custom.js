b365df6eb4bda6904b3e8cb8700abeb7628f6277
desipossa <c1jang@hanmail.net>
2024-05-27 16:09:22 +0900

    basic plant ytp slick

---
+++ js/custom.js
@@ -0,0 +1,33 @@
+$(function () {
+
+    $('.main_visual_slide').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        useTransform: false
+    });
+
+
+    $('#bgndVideo').YTPlayer({
+        videoURL: 'https://youtu.be/TWlRhK8Ll-Q',
+        containment: '.main_movie',
+        showControls: false,
+        playOnlyIfVisible: true,
+    });
+
+
+    let sw = true;
+
+    $('.main_movie .switch').on('click', function () {
+        $(this).toggleClass('on');
+        if (sw) {
+            $('#bgndVideo').YTPPause();
+        } else {
+            $('#bgndVideo').YTPPlay();
+        }
+        sw = !sw;
+    });
+
+})
